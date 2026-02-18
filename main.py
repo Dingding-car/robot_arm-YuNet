f796ef35dfdd98fd9afce7f52536125597b1094d
Dingding-car <2458591675@qq.com>
2026-02-18 14:28:10 +0800

    更改为YuNet模型

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import cv2
+import numpy as np
 import sys
 sys.path.append('./kinematic')
 from kinematic.arm5dof_uservo import Arm5DoFUServo
 
+from model.yunet import YuNet
 import threading
 import ctypes
 import inspect
@@ -11,62 +13,84 @@ import keyboard
 # //* 舵机串口号
 SERVO_PORT = 'COM8'
 
-# 线程结束代码
-def _async_raise(tid, exctype):
-    tid = ctypes.c_long(tid)
-    if not inspect.isclass(exctype):
-        exctype = type(exctype)
-    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
-    if res == 0:
-        raise ValueError("invalid thread id")
-    elif res != 1:
-        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
-        raise SystemError("PyThreadState_SetAsyncExc failed")
-        
-def stop_thread(thread):
-    _async_raise(thread.ident, SystemExit)
+# 人脸检测可视化函数
+def visualize(image, results, box_color=(0, 255, 0), text_color=(0, 0, 255), fps=None):
+    output = image.copy()
+    landmark_color = [
+        (255,   0,   0), # right eye
+        (  0,   0, 255), # left eye
+        (  0, 255,   0), # nose tip
+        (255,   0, 255), # right mouth corner
+        (  0, 255, 255)  # left mouth corner
+    ]
 
+    if fps is not None:
+        cv2.putText(output, 'FPS: {:.2f}'.format(fps), (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color)
+
+    for det in results:
+        bbox = det[0:4].astype(np.int32)
+        cv2.rectangle(output, (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3]), box_color, 2)
+
+        conf = det[-1]
+        cv2.putText(output, '{:.4f}'.format(conf), (bbox[0], bbox[1]+12), cv2.FONT_HERSHEY_DUPLEX, 0.5, text_color)
+
+        landmarks = det[4:14].astype(np.int32).reshape((5,2))
+        for idx, landmark in enumerate(landmarks):
+            cv2.circle(output, landmark, 2, landmark_color[idx], 2)
+
+    return output
+
+
+def capture_video():
+
+    # YuNet模型初始化
+    model_path = './model/face_detection_yunet_2023mar.onnx'
+    model = YuNet(modelPath=model_path,
+                  inputSize=[320, 320],
+                  confThreshold=0.9,
+                  nmsThreshold=0.3,
+                  topK=5000,
+    )
+
+    deviceId = 0 # 摄像头设备ID
+    cap = cv2.VideoCapture(deviceId)
+
+    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    model.setInputSize([w, h])
+
+    tm = cv2.TickMeter()
+
+    print('Press "q" to quit the demo.')
+    while cv2.waitKey(1) != ord('q'):
+        hasFrame, frame = cap.read()
+        if not hasFrame:
+            print('No frames grabbed!')
+            break
+
+        # Inference
+        tm.start()
+        results = model.infer(frame) # results is a tuple
+        tm.stop()
+
+        # Draw results on the input image
+        frame = visualize(frame, results, fps=tm.getFPS())
+
+        # Visualize results in a new Window
+        cv2.imshow('YuNet Demo', frame)
+
+        tm.reset()
+    
+    # 释放资源（必须执行，否则会导致摄像头被占用）
+    cap.release()  # 释放摄像头
+    cv2.destroyAllWindows()  # 关闭所有OpenCV窗口
 
 # 主函数
-
 def main():
     servo_manager = Arm5DoFUServo(device=SERVO_PORT, is_init_pose= False)
     servo_manager.home()
-    # servo_manager.set_damping(1000)
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        print("错误：无法打开摄像头！")
-        return
-    
-    print("摄像头已打开，按 'q' 键退出")
-    
-    # 2. 循环读取视频帧并显示
-    while True:
-        # 读取一帧画面
-        # ret: 布尔值，是否成功读取帧
-        # frame: 读取到的帧数据（numpy数组格式）
-        ret, frame = cap.read()
-        
-        # 如果读取失败（比如摄像头被断开），退出循环
-        if not ret:
-            print("错误：无法读取视频帧！")
-            break
-        
-        # 显示帧画面
-        # 第一个参数：窗口名称
-        # 第二个参数：要显示的帧数据
-        cv2.imshow('Camera Video', frame)
-        
-        # 3. 监听键盘事件，控制退出
-        # waitKey(1)：等待1ms获取键盘输入，返回按键的ASCII码
-        # ord('q')：获取字符'q'的ASCII码
-        # 按q键退出循环
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    # 4. 释放资源（必须执行，否则会导致摄像头被占用）
-    cap.release()  # 释放摄像头
-    cv2.destroyAllWindows()  # 关闭所有OpenCV窗口
+    servo_manager.set_damping(1000)
+    capture_video()
 
 
 if __name__ == "__main__":
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+    # servo_manager = Arm5DoFUServo(device=SERVO_PORT, is_init_pose= False)
+    # servo_manager.home()
+    # servo_manager.set_damping(1000)
